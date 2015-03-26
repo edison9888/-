a4d05c189fc98375f16b20142445395e7c9460ea
陈雄 <feelings@217.201.139.219.broad.wh.hb.dynamic.163data.com.cn>
2015-03-26 10:00:01 +0800

    Initial Commit

---
+++ 班级管家/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  班级管家
+//
+//  Created by 陈雄 on 15/3/26.
+//  Copyright (c) 2015年 陈雄. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 班级管家/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  班级管家
+//
+//  Created by 陈雄 on 15/3/26.
+//  Copyright (c) 2015年 陈雄. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
